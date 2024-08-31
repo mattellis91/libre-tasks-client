ecfe4d3fa3c110ca6861ac9a5fd58f7b69f81432
mattellis91 <mathew.ellis2@griffithuni.edu.au>
2024-09-01 09:31:29 +1000

    replaced client boilerplate

--- src/components/index.ts
+++
@@ -1,6 +0,0 @@
-import Header from "./Header/Header";
-import Sidebar from "./Sidebar/Sidebar";
-import Board from "./Board/Board";
-import EmptyBoard from "./Board/EmptyBoard";
-
-export { Header, Sidebar, Board, EmptyBoard };
